f9e8f662e7b263dc970d3e855a0df22c256f26e0
Kajal <Kajalchandel2001@gmail.com>
2025-11-01 13:31:01 +0530

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import './home.css';
 const Home = () =>{
     return(
         <section>
-            <div>
+            <div  style={{width:'100%',height:'550px',textAlign:'bottom'}} className="image">
     <h1>Welcome to our website</h1>
     <p>Looking at other people’s homepages for inspiration doesn’t lead to a successful homepage design. You need to understand the principles that underpin them.</p>     
             </div>
+            <div className="second"><h1>Start your Adventure Here</h1></div>
         </section>
     )
 }
