5e4526bf0924f57b28d625a15ee36864ce267ac1
Kajal <Kajalchandel2001@gmail.com>
2025-11-10 17:28:24 +0530

    update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,8 +6,8 @@ const Landing = () => {
 
   return (
     <section className="d-flex flex-column justify-content-center align-items-center text-center vh-100 bg-dark text-white">
-      <h1 className="fw-bold mb-3">Welcome to My Portfolio</h1>
-      <p className="text-secondary mb-4">
+      <h1 className="text-animate fw-bold mb-3">Welcome to My Portfolio</h1>
+      <p className="text-animate text-secondary mb-4">
         I’m Kajal Singh Chandel — a passionate Frontend Developer
       </p>
       
